eae161ea488c74bd59947bc413cefb8d9f5157c8
gavinwang <gavin.wang@silkrode.com.tw>
2020-05-28 11:06:01 +0800

    upload to fb and ig

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM joyzoursky/python-chromedriver:3.7-selenium 
 
 RUN mkdir -p ~/home/lib
-RUN apt-get update
-RUN apt-get install autokey-gtk -y
-RUN apt-get install dbus-x11 -y
-RUN export $(dbus-launch)
+#RUN apt-get update
+#RUN apt-get install autokey-gtk -y
+#RUN apt-get install dbus-x11 -y
+#RUN export $(dbus-launch)
 
 
 
--- main.py
+++ main.py
@@ -12,17 +12,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 
-def click_button_by_label(label:str,max_wait =10):
+def click_button_by_label(label:str,max_wait =10,show=False,element_type:str="*"):
+    driver.implicitly_wait(10)
     def _by_xpath() -> webdriver.remote.webelement.WebElement:
         driver.implicitly_wait(10)
-        return driver.find_elements_by_xpath(f"//*[text()='{label}']")
+        if show:
+            print(driver.title)
+        return driver.find_elements_by_xpath(f"//{element_type}[text()='{label}']")
 
     button = _by_xpath()
     button = button[0]
     current_wait_time_for_element_ready = 0
     while 1:
         try:
-            print(f"try click: {label}")
+            print(f"try click: {label}: {button.text}")
             button.click()
             #time.sleep(10)
             break
@@ -46,7 +49,18 @@ def click_button_by_label(label:str,max_wait =10):
             button = button[0]
 
 
+def find_xpath_key_input(xpath, content, try_option=False):
+    driver.implicitly_wait(2)
 
+    if try_option:
+        try:
+            field = driver.find_element_by_css_selector(xpath)
+            field.send_keys(content)
+        except:
+            pass
+    else:
+        field = driver.find_element_by_css_selector(xpath)
+        field.send_keys(content)
 
 
 
@@ -55,10 +69,13 @@ def click_button_by_label(label:str,max_wait =10):
 
 chrome_options = webdriver.ChromeOptions()
 
+# not capture memory
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-gpu')
+
 # show windows or not
-#chrome_options.add_argument('--no-sandbox')
-#chrome_options.add_argument('--headless')
-#chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--headless')
+
 
 #mobile mode
 #chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
@@ -73,7 +90,8 @@ prefs = {
 }
 chrome_options.add_experimental_option('prefs',prefs)
 
-driver = webdriver.Chrome(executable_path='./lib/chromedriver', chrome_options=chrome_options) if os.path.exists('./lib/chromedriver') else webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=f'{os.getcwd()}/lib/chromedriver', options=chrome_options)
+#driver = webdriver.Chrome(options=chrome_options)
 driver.set_window_size(360, 1280)
 driver.implicitly_wait(10)
 
@@ -83,12 +101,9 @@ driver.implicitly_wait(10)
 
 
 username, password = "0910365567", "!gavin840511"
-field = driver.find_element_by_css_selector("input[type='text']")
-field.send_keys(username)
-field = driver.find_element_by_css_selector("input[type='email']")
-field.send_keys(username)
-field = driver.find_element_by_css_selector("input[type='password']")
-field.send_keys(password)
+find_xpath_key_input(xpath="input[type='text']",content=username,try_option=True)
+find_xpath_key_input(xpath="input[type='email']",content=username,try_option=True)
+find_xpath_key_input(xpath="input[type='password']",content=password)
 driver.implicitly_wait(2)
 
 #button= driver.find_elements_by_xpath("//*[contains(text(), '登入')]") or driver.find_elements_by_xpath("//*[contains(text(), 'Log In')]")
@@ -101,70 +116,46 @@ driver.implicitly_wait(30)
 
 click_button_by_label(label='平行城市')
 driver.implicitly_wait(10)
+print(driver.title)
 
 click_button_by_label(label='發佈工具')
 driver.implicitly_wait(10)
+print(driver.title)
 
-click_button_by_label(label='建立貼文')
+click_button_by_label(label='建立貼文', show=True)
 driver.implicitly_wait(10)
+print(driver.title)
 
 
+
+field = driver.find_element_by_xpath(f"//*[contains(@aria-label, \"撰寫貼文\")]")
+field.click()
+field.send_keys("我最愛的工作機ＱＱ")
+driver.implicitly_wait(10)
 
 click_button_by_label(label='相片／影片')
-driver.implicitly_wait(10)
-
-
-<input accept="video/*,  video/x-m4v, video/webm, video/x-ms-wmv, video/x-msvideo, video/3gpp, video/flv, video/x-flv, video/mp4, video/quicktime, video/mpeg, video/ogv, .ts, .mkv, image/*, image/heic, image/heif" multiple="" name="composer_photo" display="inline-block" type="file" class="_n _5f0v" id="js_1y">
-
-stop
-field = driver.find_element_by_name(name='composer_photo')
+print(driver.title)
 driver.implicitly_wait(10)
 
 
 
-button=driver.find_elements_by_xpath("//*[contains(text(), '稍後再說')]")
-if len(button) > 0:
-    button[0].click()
 
-driver.implicitly_wait(5)
+"""
+##<input accept="video/*,  video/x-m4v, video/webm, video/x-ms-wmv, video/x-msvideo, video/3gpp, video/flv, video/x-flv, video/mp4, video/quicktime, video/mpeg, video/ogv, .ts, .mkv, image/*, image/heic, image/heif" multiple="" name="composer_photo" display="inline-block" type="file" class="_n _5f0v" id="js_1y">
+##field = driver.find_element_by_name(name='composer_photo')
+"""
 
-button=driver.find_elements_by_xpath("//*[contains(text(), 'Cancel')]")
-if len(button) > 0:
-    button[0].click()
-button=driver.find_elements_by_xpath("//*[contains(text(), '取消')]")
-if len(button) > 0:
-    button[0].click()
+driver.find_element_by_name("composer_photo").send_keys(os.getcwd()+"/lib/img/sample.jpg")
+time.sleep(10)
 
-driver.implicitly_wait(5)
 
-button = driver.find_elements_by_css_selector('[aria-label="New Post"]')
-if len(button) > 0:
-    button[0].click()
-button = driver.find_elements_by_css_selector('[aria-label="新貼文"]')
-if len(button) > 0:
-    button[0].click()
-driver.implicitly_wait(2)
-
-os.system('autokey-run -s select_image')
+click_button_by_label(label='貼文將會顯示在 Instagram')
 driver.implicitly_wait(10)
-button=driver.find_elements_by_xpath("//*[contains(text(), 'Expand')]")
-if len(button) > 0:
-    button[0].click()
-    driver.implicitly_wait(10)
-button=driver.find_elements_by_xpath("//*[contains(text(), 'Next')]")
-button[0].click()
 
+click_button_by_label(label='立即分享', element_type="span")
 driver.implicitly_wait(10)
-field = driver.find_elements_by_tag_name('textarea')[0]
-field.click()
-description = 'hello World!'
-field.send_keys(description)
 
-driver.implicitly_wait(15)
-button=driver.find_elements_by_xpath("//*[contains(text(), 'Share')]")
-button[-1].click()
-
-
+time.sleep(10)
 print('Success!')
 driver.implicitly_wait(15)
 driver.quit()
