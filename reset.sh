71268a396bc30fb98a0b1b8fcad0ea5a38563f22
gavinwang <gavin.wang@silkrode.com.tw>
2020-07-13 09:14:02 +0800

    debug(dicker): expand shm size of linux  .

--- reset.sh
+++ reset.sh
@@ -6,4 +6,4 @@ docker container rm ig_process_container
 docker image rm ig_process_image_py37
 
 docker build ./ --tag ig_process_image_py37
-docker run -it --name ig_process_container ig_process_image_py37
+docker run -it --shm-size 2g --name ig_process_container ig_process_image_py37
